f137c5bf8c4a5f1ee70006df1b83eb5b69e035b3
tivrama <tivrama@yahoo.com>
2016-06-06 14:51:20 -0700

    Can add actual palindromes to collection.  Next step: display collection

--- Emordnilap/public/js/controllers/MainCtrl.js
+++ Emordnilap/public/js/controllers/MainCtrl.js
@@ -1,6 +1,16 @@
 // public/js/controllers/MainCtrl.js
-angular.module('MainCtrl', []).controller('MainController', function($scope, $http) {
+angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-    // $scope.tagline = 'TODO: This will be a list of submitted palindromes from the MongoDB';
+    $scope.tagline = 'TODO: This will be a list of submitted palindromes from the MongoDB';
+
+
+
+    
+  //   $scope.tagline = [
+  //   {'name': 'dog'},
+  //   {'name': 'cat'},
+  //   {'name': 'ffffffff'}
+  // ];
+
 
 });
--- Emordnilap/public/js/controllers/NerdCtrl.js
+++ Emordnilap/public/js/controllers/NerdCtrl.js
@@ -5,22 +5,59 @@ angular.module('NerdCtrl', []).controller('NerdController', function($scope, Ner
 
 
     $scope.tagline = 'TODO: comment appears on good palendrome, or try again + see yours on the home page';
-    $scope.successprize = 'TODO: API call to play BOB video on successful submission over 15 letters';
 
-    //add functions to test if submission is a palindrome.  
+    //add functions to test if submission is a palindrome.
+    $scope.palincollection = [
+      {'entry': 'dog'},
+      {'entry': 'cat'},
+      {'entry': 'cccccc'}
+    ]
+
+    var isItPalindrome = function(word) {
+      word = word.toLowerCase().replace(/\s+/g, '');
+      var arr = word.split('').reverse().join('');
+      return word === arr;
+    };
+
+    //call isItPalindrome to see if enty is correct.
+    $scope.check = function(){
+      if (isItPalindrome($scope.userEntry)) {
+        $scope.palincollection.push(
+          {'entry': $scope.userEntry}
+          );
+        console.log($scope.palincollection)
+      }
+      //TODO: find a way to empty entry field
+
+    };
 
 
+    $scope.listpalindromes = 'hello';
+
+
+
+
+
+
+
+//----------------------------
   //it will go inside if statement that first varifies a true palindrome
-  //this will send submission to server.  Do not use yet...
-  $scope.nerd = {};
-  $scope.addNerd = function () {
-    Nerd.create($scope.nerd)
-      .catch(function (err) {
-        console.log(err);
-      });
-  };
+  //this will send submission to server.  Blow doe succescully hit the server as of june 6 am.  Do not use yet...
+  // $scope.nerd = {};
+  // $scope.addNerd = function () {
+  //   Nerd.create($scope.nerd)
+  //     .catch(function (err) {
+  //       console.log(err);
+  //     });
+  // };
 
 
+
+
+
+
+//----------------------------
+    // $scope.successprize = 'TODO: API call to play BOB video on successful submission over 15 letters';
   // $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
   // .then(function(response)
   //   { 
