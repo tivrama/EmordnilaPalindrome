154e107b90fde0f8f4b836032574f3f8f2b4b104
tivrama <tivrama@yahoo.com>
2016-08-01 19:08:29 -0700

    Add async start to mongees-mongodb

--- server.js
+++ server.js
@@ -18,7 +18,18 @@ var port = process.env.PORT || 8080;
 //----------------------------
 // connect to our mongoDB database 
 // (uncomment after establishing credentials in config/db.js)
-mongoose.connect(db.url); 
+// mongoose.connect(db.url); 
+
+mongoose.connect(db.url, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + db.url + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + db.url);
+  }
+});
+
+
+
 //----------------------------
 
 // get all data/stuff of the body (POST) parameters
