401fe4ca13bdcf64f5944eb5187d9abda5872cc7
tivrama <tivrama@yahoo.com>
2016-08-08 12:57:11 -0700

    clean up clutter

--- config/db.js
+++ config/db.js
@@ -1,50 +1,5 @@
 // config/db.js
 
-  module.exports = {
-    url: process.env.MONGOLAB_URI || 'mongodb://localhost/emordnilap'
-  };
-
-// var mlab = {
-//   dbuser: process.env.MLAB_DBUSER,
-//   dbpassword: process.env.MLAB_DBPASSWORD
-// };
-
-
-// module.exports = {
-//   // url to mongo db, for server to connect
-//   url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds145415.mlab.com:45415/heroku_h31dhr65'
-// };
-
-
-
-//----------------------------------------------------------------
-// FOR THIRD PARTY DB HOSTING WITH MLAB //
-
-// FOR DEPLOYMENT
-
-
-// DEPLOYED version:
-// var mlab = {
-//   dbuser: process.env.MLAB_DBUSER,
-//   dbpassword: process.env.MLAB_DBPASSWORD
-// };
-
-
-
-// module.exports = {
-//   // url to mongo db, for server to connect
-//   url: 'mongodb://' + mlab.dbuser + ':' + mlab.dbpassword + '@ds145415.mlab.com:45415/heroku_h31dhr65'
-// };
-
-
-
-// FOR LOCAL
-
-// var mlab =  {
-//     dbuser: require('./config.js').mlab.dbuser,
-//     dbpassword: require('./config.js').mlab.dbpassword
-// 	};
-
-//   module.exports = {
-//     url: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/emordnilap'
-//   };
+module.exports = {
+  url: process.env.MONGOLAB_URI || 'mongodb://localhost/emordnilap'
+};
--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 // modules =================================================
 var express        = require('express');
 var app            = express();
-var http = require ('http');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose    	 = require('mongoose'); //added later to solve error from cmd console
