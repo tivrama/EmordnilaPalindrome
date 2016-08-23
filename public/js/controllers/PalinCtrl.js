4e76e9be531bf82148ade590ccef4400d9d9e187
tivrama <tivrama@yahoo.com>
2016-08-23 10:22:49 -0700

    Updating master

--- public/js/controllers/PalinCtrl.js
+++ public/js/controllers/PalinCtrl.js
@@ -58,11 +58,10 @@ angular.module('PalinCtrl', []).controller('PalinController', function($scope, P
     list = list.split(' ');
     var checker = true;
     var word = 0;
-
     asyncLoop(list.length, function(loop) {
       console.log('Word sent to API: ', list[word]);
-      $http.get("http://api.wordnik.com:80/v4/word.json/" + list[word] + "/examples?includeDuplicates=false&useCanonical=false&skip=0&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5").then(function(result) {
-        console.log(result.data.examples);
+      Palindrome.checkWords({word: list[word]}).then(function(result) {
+        console.log(result.data);
         word++;
         if (!result.data.examples) {
           checker = false;
