bf5d041cc06789fa82ace0164b49fc3a7c15ce52
tivrama <tivrama@yahoo.com>
2016-10-05 09:43:11 -0700

    Comments

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,7 @@
 // public/js/controllers/MainCtrl.js
 angular.module('MainCtrl', []).controller('MainController', function($scope, Palindrome) {
 
+  // This is not in current use
   $scope.tagline = 'A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. Allowances may be made for adjustments to capital letters, punctuation, and word dividers. - Wikipedia';
 
 
